6a4a8f8ab15a59574bb162d874b064802d2e822e
NoahMasterball <123579734+NoahMasterball@users.noreply.github.com>
2023-02-07 21:45:28 +0100

    insert java into html2 pages

---
+++ 20230502_english/javascript2.js
@@ -0,0 +1,33 @@
+
+/* Standartmässig nehmen wir english als sprache */
+
+var myLanguage = 'English';
+var zaehler = 0;
+
+/* Wenn nun der knopf, die wolke gedrückt wird führe die funktion aus, sonst nicht */
+function knopfhastdruckst() {
+  /* Prüfen ob der inhalt von der schublade auch variable genannt, names "zaehler" gerade (oder ungerade via else) ist) 
+     Dabei greift man auf einen kleinen Trick zurück: Teilt man eine gerade Zahl durch zwei, ist der Rest immer null. Teilt man eine ungerade Zahl durch zwei ist der Rest nie null.
+     Modulo-Operator (%)
+     Ein Rechenbeispiel:
+    50 / 25 = 2; Rest = 0 (denn 50 – 2 * 25 = 0)
+    50 / 20 = 2.5; Rest = 10 (denn 50 – 2 * 20 = 10)
+    In JS sieht das z.B. so aus:
+    (50 % 25); Ergebnis = 0 da kein rest bleibt ist die zahl gerade
+    (50 % 20); Ergebnis = 0 da ein rest bleibt ist die zahl ungerade*/
+
+  if (zaehler % 2 == 0) {
+
+    /* Da zahl gerade ist, fülle Schublade "myLanguage" mit "Deutsch", und dann rechne beim zaehler +1 damit er beim nächsten mal ungerade ist) */
+    myLanguage = 'Deutsch';
+    zaehler++;
+  } else {
+
+    /* Da zahl nicht gerade (ungerade) ist, fülle Schublade "myLanguage" mit "Englisch", und dann rechne beim zaehler +1 damit er beim nächsten mal wieder gerade ist) */
+    myLanguage = 'English';
+    zaehler++;
+  } 
+  
+    /* Ausgabe auf Webseite nur zum testen auch mit zaehlerstand*/
+  document.getElementById("seitenausgabe").innerHTML = myLanguage + "&nbsp" + zaehler;
+}
