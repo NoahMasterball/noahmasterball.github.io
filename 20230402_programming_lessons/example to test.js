852feae69c3c48e24568aaa61141d7035f0068a6
NoahMasterball <123579734+NoahMasterball@users.noreply.github.com>
2023-04-09 14:34:40 +0200

    nr5

--- 20230402_programming_lessons/example to test.js
+++ 20230402_programming_lessons/example to test.js
@@ -2,3 +2,16 @@ alert("hello");
 alert("This is alert");
 ["This is forEach"].forEach(alert);
 console.log("And this is console.log!");
+console.warn("And this is console.warn!");
+console.error("And this is console.error!");
+if (confirm("Is this alert?")) {
+  console.log("correct");
+  alert("correct");
+} else {
+  console.log("false");
+  alert("false");
+}
+let user = prompt("What is your name?", "this is prompt");
+
+// let user = "noah";
+console.log("%c hey there " + user, "color: lime; font-size: 20px; font-weight: bold", user);
