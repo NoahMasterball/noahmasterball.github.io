02454b0c5e648c25297dcec11b34aa5be0d90638
NoahMasterball <123579734+NoahMasterball@users.noreply.github.com>
2023-05-31 11:01:03 +0200

    nr8
    not finito yet

--- 20230402_programming_lessons/javascript.js
+++ 20230402_programming_lessons/javascript.js
@@ -1,1 +1,13 @@
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 / 2);
+console.log(5 * 2);
 
+console.log(5 % 2);
+console.log(5 ** 2);
+
+let score = 99;
+
+console.log(score);
+console.log(score--);
+console.log(score);
