064405f2d501ab6be7ca1e789ee2ea77d8c7afcb
NoahMasterball <123579734+NoahMasterball@users.noreply.github.com>
2023-06-25 13:22:16 +0200

    lesson 8

--- 20230402_programming_lessons/javascript.js
+++ 20230402_programming_lessons/javascript.js
@@ -8,6 +8,13 @@ console.log(5 ** 2);
 
 let score = 99;
 
-console.log(score);
+/*console.log(score);
 console.log(score--);
-console.log(score);
+console.log(score);*/
+
+let balance = 2000;
+balance *= 1000;
+balance -= 300;
+balance /= 300;
+balance %= 2;
+console.log(balance);
