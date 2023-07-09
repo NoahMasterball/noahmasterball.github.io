9f453af87120efc87664ac022492e8dd2873b1c0
NoahMasterball <123579734+NoahMasterball@users.noreply.github.com>
2023-07-09 11:55:39 +0200

    Update votescriptextension.js

--- 20230707_votescript/votescriptextension.js
+++ 20230707_votescript/votescriptextension.js
@@ -1,13 +1,18 @@
+//
 // Dieses Sktipt is nur zur Verwendung für klicks die ohnehin selbst manuell abgesetzt würden gedacht!
 // Bei längerem laufenlassen um seine Hand zu schonen - müssen evlt. die Energieeinstellung des
+//
 // PC's so eingestellt werden dass der PC nicht in Stromsparmodus geht (suche nach Energiesparplan / Energie) 
+//
 // Rufe die Seite zum Voten auf: https://www.antenne.de/programm/aktionen/pausenhofkonzerte/schulen/13379-korbinian-aigner-gymnasium-erding
-// Stimme einmal normal ab (mit der maus), solange bist du am erneut abstimmen fenster bist
-// Dann Klicke auf die Webseite mit der rechten Maustaste -> Klicke auf "Untersuchen"    (Oder drücke die Tasten Strg + Shift + I gleichzeitig)
+// OHNE ABSTIMMEN:
+// Klicke auf die Webseite mit der rechten Maustaste -> Klicke auf "Untersuchen"    (Oder drücke die Tasten Strg + Shift + I gleichzeitig)
 // Klicke dann auf der rechten Seite oben auf "Konsole"
 // Kopiere und füge dass Script hier in die Konsole (unten bei letzte Zeile bei >) ein mit Strg + V oder der rechten Maustaste "Einfügen"
 // Drücke einmal Return/Enter
-// Lass die Seite so stehen (mit Konsole) und tue nichts mehr - sofort und danach alle 110 Sekunden stimmt dass Program automatisch ab.
+// Lass die Seite so stehen (mit Konsole) und tue nichts mehr - sofort und danach alle 120 Sekunden stimmt dass Program automatisch ab.
+// Nach ein paar stunden das browserfentster schliessen und neu starten!
+//
 function voteAgain() {
   console.clear(); console.log(""); console.log("1a. Versuche den Erneut Abstimmen Button zu drücken!"); console.log("");
   var elementExists = document.getElementById("voteIntendButton");
@@ -36,52 +41,24 @@ function counter() {
 console.clear(); console.log(""); console.log("Bisher wurde schon " + i + " mal automatisch abgestimmt!"); console.log("");
 i++;}
 function startzeitinfo() {
-  setTimeout(function() { zeitinfo(70); }, 40000); setTimeout(function() { zeitinfo(50); }, 60000);
-  setTimeout(function() { zeitinfo(30); }, 80000); setTimeout(function() { zeitinfo(10); }, 100000);
-  setTimeout(function() { zeitinfo(9); }, 101000); setTimeout(function() { zeitinfo(6); }, 104000);
-  setTimeout(function() { zeitinfo(3); }, 107000); setTimeout(function() { zeitinfo(2); }, 108000);
-  setTimeout(function() { zeitinfo(1); }, 109000); 
+  setTimeout(function() { zeitinfo(80); }, 50000); setTimeout(function() { zeitinfo(60); }, 60000);
+  setTimeout(function() { zeitinfo(40); }, 80000); setTimeout(function() { zeitinfo(20); }, 100000);
+  setTimeout(function() { zeitinfo(9); }, 111000); setTimeout(function() { zeitinfo(6); }, 114000);
+  setTimeout(function() { zeitinfo(3); }, 117000); setTimeout(function() { zeitinfo(2); }, 118000);
+  setTimeout(function() { zeitinfo(1); }, 119000); 
 }
 function startabstimmen() {
 startzeitinfo();
 setTimeout(voteAgain, 2000);
 setTimeout(voteCaptcha, 5000);
-setTimeout(vote, 39000);
+setTimeout(vote, 49000);
 setTimeout(counter, 100000);
 }
-setInterval(startabstimmen, 110000);
+setInterval(startabstimmen, 120000);
 startzeitinfo();
-voteAgain(); 
-console.log(""); console.log("Automatisches Abstimmungsprogramm wurde gestartet...nächster Start in 110 Sekunden...nur die Wartzeit der ersten automatischen Abstimmung dauert etwas lang...danach geht es schneller..."); console.log("");
+startabstimmen(); 
+console.log(""); console.log("Automatisches Abstimmungsprogramm wurde gestartet...erster vote started gleich"); console.log("");
 //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
