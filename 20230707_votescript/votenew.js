ba0cf06dc1e42dad3c756cf47c429a2e4096e6d6
NoahMasterball <123579734+NoahMasterball@users.noreply.github.com>
2023-07-11 11:26:46 +0200

    Update votenew.js

--- 20230707_votescript/votenew.js
+++ 20230707_votescript/votenew.js
@@ -1,3 +1,18 @@
+//
+// Dieses Sktipt is nur zur Verwendung für klicks die ohnehin selbst manuell abgesetzt würden gedacht!
+// Bei längerem laufenlassen um seine Hand zu schonen - müssen evlt. die Energieeinstellung des
+//
+// PC's so eingestellt werden dass der PC nicht in Stromsparmodus geht (suche nach Energiesparplan / Energie) 
+//
+// Rufe die Seite zum Voten auf: https://www.antenne.de/programm/aktionen/pausenhofkonzerte/schulen/13379-korbinian-aigner-gymnasium-erding
+// OHNE ABSTIMMEN:
+// Klicke auf die Webseite mit der rechten Maustaste -> Klicke auf "Untersuchen"    (Oder drücke die Tasten Strg + Shift + I gleichzeitig)
+// Klicke dann auf der rechten Seite oben auf "Konsole" falls nicht sichbar -> Menue erweitern mit >>
+// Kopiere und füge dass Script in die Konsole des Browsers ein (unten in der letzten Zeile bei >) entweder mit Strg + V oder der rechten Maustaste "Einfügen"
+// Drücke einmal Return/Enter
+// Lass die Seite so stehen (mit Konsole) und tue nichts mehr - ab jetzt funtkioniert alles automatisch
+// Nach ein paar Stunden das browserfenster schliessen und neu starten!
+//
 function start () {
 
         var output ="";
@@ -85,11 +100,9 @@ function start () {
           } else {
             output = output + "\n   Nein: Button Cookies Ablehnen nicht gefunden"
           }
-   
             // Den nächsten Aufruf von start() zufällig zwischen 1.5 und 5.2 sekunden
             var randomDelay = Math.random() * (3.8 - 1.5) + 2;
-
-            // Den nächsten Aufruf von start() den evtl. vorhandenen Zähler addieren
+            // Den nächsten Aufruf von start() Zähler addieren
             if (abstimmung == "ja") {
                 randomDelay += 60;
               abstimmung = "nein";
@@ -102,19 +115,4 @@ function start () {
         console.log("\n\n\n\n\n" + output + "\n\n\n\n\n")   
 }
 start();
-
-
- /*
-        <button type="submit" id="voteIntendButton" class="c-button c-button--large c-button--centered u-extra-small-margin--top ">Jetzt abstimmen</button>
-        <button type="submit" id="voteIntendButton" class="c-button c-button--large c-button--centered u-extra-small-margin--top ">Jetzt abstimmen</button>
-
-        Anti Robotor seite den Akzeptieren & Zustimmen button gefunden:
-        Via: document.querySelector('label.c-embed__optinbutton.c-button.has-clickhandler[style="--button-width:auto"][data-a11y-dialog-hide=""]');
-
-        <label class="c-embed__optinbutton c-button has-clickhandler" style="--button-width:auto" data-a11y-dialog-hide="">
-            <div class="uc-embed" uc-layout="serviceDetails" uc-consent-name="Friendly Captcha" uc-data="optInCheckboxWithLabel"><div class="uc uc-checkbox uc-optInCheckboxWithLabel" style="display: inline-block"><input type="checkbox" id="Friendly Captcha" title="Friendly Captcha"><label class="uc uc-title uc-optInCheckboxWithLabel" style="display: inline-block" for="Friendly Captcha"><h3>Friendly Captcha</h3></label></div><div class="uc uc-furtherInformation uc-subservices service-specific" style="padding: 10px 20px 0"></div></div>
-            Akzeptieren &amp; Akzeptieren &amp; Sicherheitsprüfung aktivieren
-        </label>
-
-
-        */
+//
