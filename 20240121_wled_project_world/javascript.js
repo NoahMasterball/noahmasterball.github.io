35ff276a38c5e28879826a07f38828406ef45e21
NoahMasterball <123579734+NoahMasterball@users.noreply.github.com>
2024-02-15 19:03:28 +0100

    add API JS

--- 20240121_wled_project_world/javascript.js
+++ 20240121_wled_project_world/javascript.js
@@ -1,59 +1,39 @@
-// prettier-ignore
-window.addEventListener("load", function () {
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        document.getElementById("rocket1t").addEventListener("mousedown", function() {mouseOver1("rocket1.jpg")});
-          document.getElementById("rocket2t").addEventListener("mousedown", function() {mouseOver1("rocket2.jpg")});
-          document.getElementById("rocket3t").addEventListener("mousedown", function() {mouseOver1("rocket3.jpg")});
-          document.getElementById("rocket4t").addEventListener("mousedown", function() {mouseOver1("rocket4.jpg")});
-          document.getElementById("rocket5t").addEventListener("mousedown", function() {mouseOver1("rocket5.jpg")});
-          document.getElementById("mobile1t").addEventListener("mousedown", function() {mouseOver1("mobile1.jpg")});
-          document.getElementById("mobile2t").addEventListener("mousedown", function() {mouseOver1("mobile2.jpg")});
-          document.getElementById("mobile3t").addEventListener("mousedown", function() {mouseOver1("mobile3.jpg")});
-          document.getElementById("mobile4t").addEventListener("mousedown", function() {mouseOver1("mobile41.jpg")});
-          document.getElementById("rocketseepictext").addEventListener("mouseover", function() {mouseOver2()});
-          document.getElementById("linktextspace").addEventListener("mouseover", function() {mouseOver3()});
-          document.getElementById("englishvoclink").addEventListener("mouseover", function() {mouseOver3()});
-    }else{
-          document.getElementById("rocket1").addEventListener("mousedown", function() {mouseOver1("rocket1.jpg")});
-          document.getElementById("rocket2").addEventListener("mousedown", function() {mouseOver1("rocket2.jpg")});
-          document.getElementById("rocket3").addEventListener("mousedown", function() {mouseOver1("rocket3.jpg")});
-          document.getElementById("rocket4").addEventListener("mousedown", function() {mouseOver1("rocket4.jpg")});
-          document.getElementById("rocket5").addEventListener("mousedown", function() {mouseOver1("rocket5.jpg")});
-          document.getElementById("rocket1t").addEventListener("mousedown", function() {mouseOver1("rocket1.jpg")});
-          document.getElementById("rocket2t").addEventListener("mousedown", function() {mouseOver1("rocket2.jpg")});
-          document.getElementById("rocket3t").addEventListener("mousedown", function() {mouseOver1("rocket3.jpg")});
-          document.getElementById("rocket4t").addEventListener("mousedown", function() {mouseOver1("rocket4.jpg")});
-          document.getElementById("rocket5t").addEventListener("mousedown", function() {mouseOver1("rocket5.jpg")});
-          document.getElementById("mobile1").addEventListener("mousedown", function() {mouseOver1("mobile1.jpg")});
-          document.getElementById("mobile2").addEventListener("mousedown", function() {mouseOver1("mobile2.jpg")});
-          document.getElementById("mobile3").addEventListener("mousedown", function() {mouseOver1("mobile3.jpg")});
-          document.getElementById("mobile4").addEventListener("mousedown", function() {mouseOver1("mobile4.jpg")});
-          document.getElementById("mobile1t").addEventListener("mousedown", function() {mouseOver1("mobile1.jpg")});
-          document.getElementById("mobile2t").addEventListener("mousedown", function() {mouseOver1("mobile2.jpg")});
-          document.getElementById("mobile3t").addEventListener("mousedown", function() {mouseOver1("mobile3.jpg")});
-          document.getElementById("mobile4t").addEventListener("mousedown", function() {mouseOver1("mobile4.jpg")});
-          document.getElementById("rocketseepictext").addEventListener("mouseover", function() {mouseOver2()});
-          document.getElementById("linktextspace").addEventListener("mouseover", function() {mouseOver3()});
-          document.getElementById("englishvoclink").addEventListener("mouseover", function() {mouseOver3()});
-    }
-          document.getElementById("mtext").addEventListener("mouseover", function() {mouseOver4()});
-          document.getElementById("linktextenglish").addEventListener("mouseover", function() {mouseOver5()});
-});
 
-function mouseOver1(filename) {
-  window.open("./index/" + encodeURI(filename));
+// Ein Objekt mit den Daten, die gesendet werden sollen
+const data = {"on":true,"bri":10,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":16,"startY":0,"stopY":8,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"n":"Gute Nacht","col":[[255,0,0],[0,0,0],[0,0,0]],"fx":183,"sx":128,"ix":128,"pal":11,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"rY":false,"mY":false,"tp":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]};
+
+// Ein Objekt mit den Optionen für die fetch-Anfrage
+const options = {
+  method: "POST", // Die Methode zum Senden der Daten
+  headers: {
+    "Content-Type": "application/json" // Der Content-Type der Daten
+  },
+  body: JSON.stringify(data) // Der JSON-String der Daten
+};
+// Die URL der API
+const url2 = "http://192.168.179.29/json/state";
+// Die fetch-Anfrage mit den Optionen ausführen
+function fetchData2() {
+    // Ein fetch-Aufruf, der die Antwort in ein Objekt umwandelt und in console.log ausgibt
+    fetch(url2, options)
+      .then(response => response.json()) // Die Antwort in ein Objekt umwandeln
+      .then(data => console.log(data)) // Das Objekt in der Konsole ausgeben
+      .catch(error => console.error(error)); // Den Fehler abfangen und ausgeben
+    }
+// Die Funktion als Callback an das Ereignis window.onload übergeben
+window.onload = fetchData2;
+    
+
+// Eine Funktion, die den fetch-Aufruf ausführt
+function fetchData() {
+  // Ein fetch-Aufruf, der die Antwort in ein Objekt umwandelt und in console.log ausgibt
+  fetch('http://192.168.179.29/json/effects')
+    .then(response => response.json()) // Die Antwort in ein Objekt umwandeln
+    .then(data => console.log(data)) // Das Objekt in console.log ausgeben
+    .catch(error => console.error(error)); // Fehlerbehandlung
 }
-function mouseOver2() {
-  document.getElementById("rocketpicunsichtbar").classList.replace("hidden", "visible");
-}
-function mouseOver3() {
-  document.getElementById("rocketpicunsichtbar").classList.replace("visible", "hidden");
-}
-function mouseOver4() {
-  document.getElementById("hidden").classList.replace("hidden", "visible");
-  console.log("ausgelöst");
-}
-function mouseOver5() {
-  document.getElementById("hidden").classList.replace("visible", "hidden");
-  console.log("ausgelösttext");
-}
+// Die Funktion als Callback an das Ereignis window.onload übergeben
+// window.onload = fetchData;
+setTimeout(fetchData, 2000);
+
+
