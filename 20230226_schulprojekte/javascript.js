2c7270c4ff9ee7b609adb0535541f360439c3e47
NoahMasterball <123579734+NoahMasterball@users.noreply.github.com>
2024-04-29 20:10:58 +0200

    vocume

--- 20230226_schulprojekte/javascript.js
+++ 20230226_schulprojekte/javascript.js
@@ -1,34 +1,19 @@
-
-
-var einheit = " m"
-
+let einheit = "m"
+let qwert = "m"
+let zwert = "m"
 
 document.getElementById("calc").addEventListener("click", function() {
-  var lengthInput = document.getElementById('field1');
   console.log(parseInt(field1.value)+parseInt(field2.value));
-  document.getElementById("ergebnis").innerHTML = "Ergebnis:  " + field1.value + " " + einheit + "<sup>3</sup>";
+  let nen1 = field1.value * field2.value  * field3.value
+  document.getElementById("ergebnis").innerHTML = "Ergebnis:  " + nen1 + " " + einheit + "<sup>3</sup>";
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 document.getElementById("b1").addEventListener("click", function() {
   for (var i = 1; i <= 9; i++) {
     document.getElementById("b" + i).style.color = "black";
   }
   document.getElementById("b1").style.color = "#fff";
+  einheit = "nm"
 });
 
 document.getElementById("b2").addEventListener("click", function() {
@@ -36,6 +21,7 @@ document.getElementById("b2").addEventListener("click", function() {
     document.getElementById("b" + i).style.color = "black";
   }
   document.getElementById("b2").style.color = "#fff";
+  einheit = "μm"
 });
 
 document.getElementById("b3").addEventListener("click", function() {
@@ -43,6 +29,7 @@ document.getElementById("b3").addEventListener("click", function() {
     document.getElementById("b" + i).style.color = "black";
   }
   document.getElementById("b3").style.color = "#fff";
+  einheit = "mm"
 });
 
 document.getElementById("b4").addEventListener("click", function() {
@@ -50,6 +37,7 @@ document.getElementById("b4").addEventListener("click", function() {
     document.getElementById("b" + i).style.color = "black";
   }
   document.getElementById("b4").style.color = "#fff";
+  einheit = "cm"
 });
 
 document.getElementById("b5").addEventListener("click", function() {
@@ -57,6 +45,7 @@ document.getElementById("b5").addEventListener("click", function() {
     document.getElementById("b" + i).style.color = "black";
   }
   document.getElementById("b5").style.color = "#fff";
+  einheit = "dm"
 });
 
 document.getElementById("b6").addEventListener("click", function() {
@@ -64,6 +53,7 @@ document.getElementById("b6").addEventListener("click", function() {
     document.getElementById("b" + i).style.color = "black";
   }
   document.getElementById("b6").style.color = "#fff";
+  einheit = "m"
 });
 
 document.getElementById("b7").addEventListener("click", function() {
@@ -71,6 +61,7 @@ document.getElementById("b7").addEventListener("click", function() {
     document.getElementById("b" + i).style.color = "black";
   }
   document.getElementById("b7").style.color = "#fff";
+  einheit = "dam"
 });
 
 document.getElementById("b8").addEventListener("click", function() {
@@ -78,6 +69,7 @@ document.getElementById("b8").addEventListener("click", function() {
     document.getElementById("b" + i).style.color = "black";
   }
   document.getElementById("b8").style.color = "#fff";
+  einheit = "hm"
 });
 
 document.getElementById("b9").addEventListener("click", function() {
@@ -85,6 +77,13 @@ document.getElementById("b9").addEventListener("click", function() {
     document.getElementById("b" + i).style.color = "black";
   }
   document.getElementById("b9").style.color = "#fff";
+  einheit = "km"
 });
 
-
+document.getElementById("q1").addEventListener("click", function() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById("q" + i).style.color = "black";
+  }
+  document.getElementById("q1").style.color = "#fff";
+  qwert = "-9"
+});
