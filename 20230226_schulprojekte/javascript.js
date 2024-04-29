33b696683c1960e51acc36005b416b4c9b6fd60a
NoahMasterball <123579734+NoahMasterball@users.noreply.github.com>
2024-04-29 16:34:49 +0200

    volume school project

---
+++ 20230226_schulprojekte/javascript.js
@@ -0,0 +1,63 @@
+
+document.getElementById("b1").addEventListener("click", function() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById("b" + i).style.color = "black";
+  }
+  document.getElementById("b1").style.color = "#fff";
+});
+
+document.getElementById("b2").addEventListener("click", function() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById("b" + i).style.color = "black";
+  }
+  document.getElementById("b2").style.color = "#fff";
+});
+
+document.getElementById("b3").addEventListener("click", function() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById("b" + i).style.color = "black";
+  }
+  document.getElementById("b3").style.color = "#fff";
+});
+
+document.getElementById("b4").addEventListener("click", function() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById("b" + i).style.color = "black";
+  }
+  document.getElementById("b4").style.color = "#fff";
+});
+
+document.getElementById("b5").addEventListener("click", function() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById("b" + i).style.color = "black";
+  }
+  document.getElementById("b5").style.color = "#fff";
+});
+
+document.getElementById("b6").addEventListener("click", function() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById("b" + i).style.color = "black";
+  }
+  document.getElementById("b6").style.color = "#fff";
+});
+
+document.getElementById("b7").addEventListener("click", function() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById("b" + i).style.color = "black";
+  }
+  document.getElementById("b7").style.color = "#fff";
+});
+
+document.getElementById("b8").addEventListener("click", function() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById("b" + i).style.color = "black";
+  }
+  document.getElementById("b8").style.color = "#fff";
+});
+
+document.getElementById("b9").addEventListener("click", function() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById("b" + i).style.color = "black";
+  }
+  document.getElementById("b9").style.color = "#fff";
+});
